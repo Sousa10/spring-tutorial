82f9adb65edeb1324a5b74cd851d0c88a979e6fa
Rui Matias Silva de Sousa <ruisousa05@gmail.com>
2022-10-26 09:57:39 -0100

    initial commit

---
+++ src/main/kotlin/com/example/tutorial/ruitutorial/RuitutorialApplication.kt
@@ -0,0 +1,11 @@
+package com.example.tutorial.ruitutorial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RuitutorialApplication
+
+fun main(args: Array<String>) {
+	runApplication<RuitutorialApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/tutorial/ruitutorial/RuitutorialApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.tutorial.ruitutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RuitutorialApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
